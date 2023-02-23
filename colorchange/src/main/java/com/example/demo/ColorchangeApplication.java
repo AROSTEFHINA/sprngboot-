6154ecc80e264f13a64599ee812fdb8d3e3b9b8e
AROSTEFHINA <113664246+AROSTEFHINA@users.noreply.github.com>
2023-02-23 18:48:43 +0530

    Add files via upload

---
+++ colorchange/src/main/java/com/example/demo/ColorchangeApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColorchangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ColorchangeApplication.class, args);
+	}
+
+}
